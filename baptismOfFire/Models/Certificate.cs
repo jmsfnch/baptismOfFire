3da710f3c7fb2341f2de25739c386a817d4ce0a8
James Finch <jmsfnch@gmail.com>
2018-12-09 14:32:16 +0000

    added certificate and server information to deployment view.
    Modified classes to provide clean timestamp formats

--- baptismOfFire/Models/Certificate.cs
+++ baptismOfFire/Models/Certificate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace baptismOfFire.Models
     {
         public int ID { get; set; }
         public string CN { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ExpiresDate { get; set; }
 
         public virtual ICollection<Deployment> Deployments { get; set; }
--- baptismOfFire/Models/Deployment.cs
+++ baptismOfFire/Models/Deployment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,7 @@ namespace baptismOfFire.Models
         public int ID { get; set; }
         public virtual Server Server { get; set; }
         public virtual Certificate Certificate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreateDate { get; set; }
     }
 }
