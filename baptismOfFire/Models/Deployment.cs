ebce66f00e0e567fd3bbb9f95b9fb8e60988cef0
James Finch <jmsfnch@gmail.com>
2018-12-09 13:32:18 +0000

    created deployment class

---
+++ baptismOfFire/Models/Deployment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baptismOfFire.Models
+{
+    public class Deployment
+    {
+        public int ID { get; set; }
+        public Server Server { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
