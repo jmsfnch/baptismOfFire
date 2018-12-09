de5411aaa2fb75b737a4e847508f517286de1098
James Finch <jmsfnch@gmail.com>
2018-12-09 13:35:45 +0000

    created certificate class

---
+++ baptismOfFire/Models/Certificate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace baptismOfFire.Models
+{
+    public class Certificate
+    {
+        public int ID { get; set; }
+        public string CN { get; set; }
+        public DateTime ExpiresDate { get; set; }
+
+        public virtual ICollection<Deployment> Deployments { get; set; }
+
+    }
+}
